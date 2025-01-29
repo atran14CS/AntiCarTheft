d58968b6476acda1082a9ffb693abe74dbb85b67
atran14CS <atran14@uw.edu>
2025-01-28 22:32:23 -0800

    updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import './Home.css';
-import Searchbar from '../componets/Searchbar';
+import Searchbar from '../componets/searchbar';
 import Hero from '../componets/Hero';
 
 const Home = () => {
   return (
     <div>
-      <h1>Anti Car Theft</h1>
+      <h1>AutoGuard</h1>
       <button>Upload</button>
       <button>Home</button>
       <Searchbar />
