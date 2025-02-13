f9033ebfbc6368bbf16ed06dafcb47fc82d1aaca
atran14CS <atran14@uw.edu>
2025-02-12 20:40:57 -0800

    push new

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,4 @@
+import React from 'react'; // Add this import
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
@@ -6,4 +7,4 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-);
+)
